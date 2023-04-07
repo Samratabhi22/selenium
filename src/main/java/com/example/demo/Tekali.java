eaf3193ac39f43634e80eb41652d36aada12f017
Admin <Admin@DESKTOP-P1FTGIL>
2023-04-07 22:30:51 +0530

    5th commit done in master

--- src/main/java/com/example/demo/Tekali.java
+++ src/main/java/com/example/demo/Tekali.java
@@ -4,7 +4,7 @@ public class Tekali {
 
 	public static void main(String[] args) {
 		System.out.println(" Takeli is anjalis pet name ");
-
+		System.out.println(" Takeli mother name is siya singh");
 	}
 
 }
