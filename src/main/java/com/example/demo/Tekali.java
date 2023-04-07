7d9d6562d7e64802d24c7f0e5dada0bf7d384a60
Admin <Admin@DESKTOP-P1FTGIL>
2023-04-07 22:36:39 +0530

    anjali commit

--- src/main/java/com/example/demo/Tekali.java
+++ src/main/java/com/example/demo/Tekali.java
@@ -5,6 +5,7 @@ public class Tekali {
 	public static void main(String[] args) {
 		System.out.println(" Takeli is anjalis pet name ");
 		System.out.println(" Takeli mother name is siya singh");
+		System.out.println(" Takeli is anjali");
 	}
 
 }
